65f0ae3e5dcf271f1398c26e6f3a90b23eb85b5c
marc2332 <mespinsanz@gmail.com>
2024-12-11 18:14:56 +0100

    clean up day 11 p1

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -11,26 +11,23 @@ fn main() {
         .collect::<Vec<_>>();
 
     for blink in 0..25 {
-        let mut offset = 0;
-        for (i, stone) in data.clone().into_iter().enumerate() {
-            let i = i + offset;
+        for stone in data.drain(..).collect::<Vec<_>>().into_iter() {
             let s = stone.to_string();
             match stone {
                 0 => {
-                    data[i] = 1;
+                    data.push(1);
                 }
                 _ if s.len() % 2 == 0 => {
                     let (first, second) = s.split_at(s.len() / 2);
-                    data.insert(i, first.parse().unwrap());
-                    *data.get_mut(i + 1).unwrap() = second.parse().unwrap();
-                    offset += 1;
+                    data.push(first.parse().unwrap());
+                    data.push(second.parse().unwrap());
                 }
                 _ => {
-                    data[i] = stone * 2024;
+                    data.push(stone * 2024);
                 }
             }
         }
 
-        println!("{blink}: {}", data.len())
+        println!("{blink}: {:?}", data.len())
     }
 }
